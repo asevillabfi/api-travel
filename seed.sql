209206cea8fd520cbfe3c4a2e0d7a830eb953500
asevillabfi <alyanna.sevilla@newbrighton.edu.ph>
2023-11-29 12:32:14 +0800

    add values

--- seed.sql
+++ seed.sql
@@ -10,48 +10,45 @@ CREATE DATABASE travel;
 ------------
 
 CREATE TABLE IF NOT EXISTS ticket (
-    ticket_id integer PRIMARY KEY NOT NULL,
+    ticket_id serial PRIMARY KEY NOT NULL,
     destination_id integer NOT NULL,
     user_id integer NOT NULL,
-    fName character varying(100) NOT NULL,
-    lName character varying(100) NOT NULL,
     plane_id integer NOT NULL,
     date date NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS plane (
-    plane_id integer PRIMARY KEY NOT NULL,
+    plane_id serial PRIMARY KEY NOT NULL,
     plane_code character varying(16) NOT NULL,
     plane_airline character varying(100) NOT NULL,
     route_id integer NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS route (
-    route_id integer PRIMARY KEY NOT NULL,
+    route_id serial PRIMARY KEY NOT NULL,
     route_origin character varying(25) NOT NULL,
     route_destination character varying(25) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS destination (
-    destination_id integer PRIMARY KEY NOT NULL,
+    destination_id serial PRIMARY KEY NOT NULL,
     country_name character varying(100) NOT NULL,
     city_name character varying(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS roles (
-    role_id integer PRIMARY KEY NOT NULL,
+    role_id serial PRIMARY KEY NOT NULL,
     role character(100) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id integer PRIMARY KEY NOT NULL,
+    user_id serial PRIMARY KEY NOT NULL,
     fName character(100) NOT NULL,
     lName character(100) NOT NULL,
     age integer NOT NULL,
     username character varying(100) NOT NULL,
     password character varying(100) NOT NULL,
-    role_id integer NOT NULL,
-    role character(100) NOT NULL
+    role_id integer NOT NULL
 );
 
 
@@ -73,3 +70,44 @@ ALTER TABLE ONLY public.ticket
 
 ALTER TABLE ONLY public.ticket
     ADD CONSTRAINT fk_plane_id FOREIGN KEY (plane_id) REFERENCES public.plane(plane_id);
+
+
+------------
+-- INSERT --
+------------
+
+INSERT INTO public.route(
+    route_id, route_origin, route_destination)
+VALUES (default, 'PH', 'JP'),
+       (default, 'SK', 'PH'),
+       (default, 'JP', 'SK');
+
+INSERT INTO public.plane(
+    plane_id, plane_code, plane_airline, route_id)
+VALUES (default, 'PA100', 'Philippine Airlines', 1),
+       (default, 'JA304', 'Japan Airlines', 2),
+       (default, 'KA560', 'Korean Air', 3);
+
+INSERT INTO public.roles(
+    role_id, role)
+VALUES (default, 'user'),
+       (default, 'admin');
+
+INSERT INTO public.users(
+    user_id, fname, lname, age, username, password, role_id)
+VALUES (default, 'Machi', 'Tobaye', 19, 'machiWIN', 'yuh123', 1),
+       (default, 'Kyoka', 'Hibari', 18, 'user', 'pass', 1),
+       (default, 'Neil', 'Edwards', 22, 'yuri', 'love', 1),
+       (default, 'Alexis', 'Detoyato', 22, 'admin', 'pass', 2);
+
+INSERT INTO public.destination(
+    destination_id, country_name, city_name)
+VALUES (default, 'Philippines', 'Manila'),
+       (default, 'Japan', 'Tokyo'),
+       (default, 'South Korea', 'Seoul');
+
+INSERT INTO public.ticket(
+    ticket_id, destination_id, user_id, plane_id, date)
+VALUES (default, 1, 1, 1, '2020-6-23'),
+       (default, 2, 2, 2, '2022-8-5'),
+       (default, 3, 3, 3, '2023-10-13');
